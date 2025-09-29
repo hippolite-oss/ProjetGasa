3446bc7ff14d0f70e7a4e996d7885d15a395be21
hippolite-oss <hippoliteagbodamakou@gmail.com>
2025-09-29 03:39:59 +0100

    Update file

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/header/Header';
 import Accueil from './pages/Accueil/Accueil';
+import Faculte from './pages/Faculte/Faculte';
 import Admissions from './pages/Admissions/Admissions';
 import Recherche from './pages/Recherche/Recherche';
 import Hebergement from './pages/Hebergement/Hebergement';
-import Faculte from './pages/Faculte/Faculte';
 import Informations from './pages/Informations/Informations';
+
 
 function App() {
   return (
@@ -14,13 +15,11 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Accueil />} />
+        <Route path="/Faculte" element={<Faculte/>} />
         <Route path="/Admissions" element={<Admissions />} />
         <Route path="/Recherche" element={<Recherche />} />
         <Route path="/Hebergement" element={<Hebergement />} />
         <Route path="/Informations" element={<Informations/>} />
-        <Route path="/Faculte" element={<Faculte/>} />
-        
-
       </Routes>
     </div>
   );
